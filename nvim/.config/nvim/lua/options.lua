d65fbfc7c60371ad35c052bd9309cd58b7f656ff
Rohan Simon <rohan.simon@ufl.edu>
2024-08-25 20:03:35 -0400

    initial commit

---
+++ nvim/.config/nvim/lua/options.lua
@@ -0,0 +1,32 @@
+require("remap")
+
+-- Theme
+vim.cmd.colorscheme "catppuccin-mocha"
+
+-- settings
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.updatetime = 50
+vim.opt.signcolumn = "yes"
+
+-- netrw settings
+vim.g.netrw_banner = 0
+vim.g.netrw_bufsettings = 'noma nomod nu rnu nobl nowrap ro'
+
---
+++ nvim/.config/nvim/lua/remap.lua
@@ -0,0 +1,30 @@
+-- imports
+local builtin = require('telescope.builtin')
+local harpoon = require("harpoon")
+
+-- keybinds
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+
+-- telescope
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- harpoon
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+
+-- lsp
+vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+vim.keymap.set("n", "<leader>fd", vim.lsp.buf.definition, {})
+vim.keymap.set({'n', 'v'}, "<leader>ca", vim.lsp.buf.code_action, {})
