c922f112b4648dcddeb81eb00418fe6013131eec
Rohan <rohan@Rohans-MacBook-Pro.local>
2025-08-31 13:27:38 -0400

    updates

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "pyright" }
+                ensure_installed = { "lua_ls" }
             })
         end
     },
@@ -18,33 +18,6 @@ return {
         config = function()
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({})
-            lspconfig.pyright.setup({})
-            lspconfig.clangd.setup({})
-            lspconfig.biome.setup({})
-            lspconfig.rust_analyzer.setup({})
-            lspconfig.emmet_language_server.setup({
-                filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "pug", "typescriptreact" },
-                init_options = {
-                    ---@type table<string, string>
-                    includeLanguages = {},
-                    --- @type string[]
-                    excludeLanguages = {},
-                    --- @type string[]
-                    extensionsPath = {},
-                    --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
-                    preferences = {},
-                    --- @type boolean Defaults to `true`
-                    showAbbreviationSuggestions = true,
-                    --- @type "always" | "never" Defaults to `"always"`
-                    showExpandedAbbreviation = "always",
-                    --- @type boolean Defaults to `false`
-                    showSuggestionsAsSnippets = false,
-                    --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
-                    syntaxProfiles = {},
-                    --- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
-                    variables = {},
-                },
-            })
         end
     }
 }
--- zshrc/.zshrc
+++ zshrc/.zshrc
@@ -1,18 +1,3 @@
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/Users/rhs_air/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/Users/rhs_air/anaconda3/etc/profile.d/conda.sh" ]; then
-        . "/Users/rhs_air/anaconda3/etc/profile.d/conda.sh"
-    else
-        export PATH="/Users/rhs_air/anaconda3/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-# <<< conda initialize <<<
-
 autoload -U colors && colors
 PS1="%{$fg[red]%}%n%{$reset_color%}@%{$fg[blue]%}%m %{$fg[yellow]%}%~ %{$reset_color%}%% "
 export CLICOLOR=1
@@ -20,14 +5,10 @@ export LSCOLORS=ExFxBxDxCxegedabagacad
 
 alias ls='ls -GFh'
 alias ll='ls -lrt'
-alias cdd='cd ~/desktop'
-alias yta-mp3="youtube-dl --extract-audio --audio-format mp3 "
+alias yta-mp3="youtube-dl --extract-audio --audio-format mp3"
 alias cp="cp -v"
 alias rm="rm -v"
 alias vim="nvim"
-alias imgcat="sh ~/imgcat.sh"
-alias icat="kitten icat"
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+alias lg="lazygit"
+alias ssp="source ~/.zshrc"
+alias csp="vim ~/.zshrc"
